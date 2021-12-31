90e020d6f30a9af50531e33772525d10b7abec52
xyh <906357476@qq.com>
2021-12-31 15:31:53 +0800

    v3

--- src/com/doc36/Test.java
+++ src/com/doc36/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("哈罗就阿瓦v1");
         System.out.println("哈罗就阿瓦v2");
+        System.out.println("哈罗就阿瓦v3");
     }
 }
